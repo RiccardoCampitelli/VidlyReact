ce63fd26f7e336524ad02c5841ac027400ba01e8
rcdCamp <riccardo.campitelli91@gmail.com>
2019-04-21 15:00:56 +0100

    Added Routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import React, { Component } from "react";
 import "./App.css";
 import Movies from "./Components/movies";
+import NavBar from "./Components/common/NavBar";
+import { Switch, Route, Redirect } from "react-router-dom";
+import Customers from "./Components/customers";
+import Rentals from "./Components/rentals";
+import NotFound from "./Components/not-found";
+import MovieForm from "./Components/MovieForm";
 
 class App extends Component {
   render() {
     return (
-      <main className="container">
-        <Movies />
-      </main>
+      <React.Fragment>
+        <NavBar />
+        <main className="container">
+          <Switch>
+            <Route path="/movies/:id" component={MovieForm} />
+            <Route path="/movies" component={Movies} />
+            <Route path="/customers" component={Customers} />
+            <Route path="/rentals" component={Rentals} />
+            <Route path="/not-found" component={NotFound} />
+            <Redirect from="/" to="/movies" />
+            <Redirect to="/not-found" />
+          </Switch>
+        </main>
+      </React.Fragment>
     );
   }
 }
---
+++ src/Components/MovieForm.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MovieForm = props => {
+  return <h1>Movie Form {props.match.params.id} </h1>;
+};
+
+export default MovieForm;
